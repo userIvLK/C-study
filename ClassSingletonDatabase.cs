0922d3a9682ae1691e1e3f7ccd79e33ab56ff5b7
userIvLK <121995211+userIvLK@users.noreply.github.com>
2024-03-16 09:35:00 +0300

    Add files via upload

---
+++ ClassSingletonDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_study
+{
+    internal class SingletonDatabase
+    {
+        private static string adress;
+        private static SingletonDatabase instance;
+        private SingletonDatabase(string Adress)
+        {
+            adress = Adress;
+        }
+        public static SingletonDatabase getInstance(string Adress)
+        {
+            if (instance == null)
+            {
+                instance = new SingletonDatabase(Adress);
+            }
+            return instance;
+        }
+        public void getDataFromAdress()
+        {
+            Console.WriteLine($"data taken from {adress}");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Csharp_study;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SingletonDatabase DB1 = SingletonDatabase.getInstance("abc");
+        DB1.getDataFromAdress();
+        SingletonDatabase DB2 = SingletonDatabase.getInstance("def");
+        DB1.getDataFromAdress();
+        DB2.getDataFromAdress();
+    }
+}

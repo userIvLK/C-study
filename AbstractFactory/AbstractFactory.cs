ad4d7c5b351e2f563107ac9c34e50ffbb5b06b4d
userIvLK <121995211+userIvLK@users.noreply.github.com>
2024-03-17 09:28:39 +0300

    Add files via upload

---
+++ AbstractFactory/AbstractFactory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_study
+{
+    internal abstract class AbstractFactory //Creates 2 types of ojects
+    {
+        abstract public AbstractProduct createSquare();
+        abstract public AbstractProduct createTriangle();
+    }
+    internal abstract class AbstractProduct 
+    {
+        abstract public void printShape();
+        abstract public void printColor();
+    }
+    class Square: AbstractProduct  
+    {
+        public Square(string Color) {color = Color;}
+        private string color;
+        public override void printShape()
+        {
+            Console.WriteLine("I am square");
+        }
+        public override void printColor()
+        {
+            Console.WriteLine($"I am {color}");
+        }
+    }
+    class Triangle : AbstractProduct
+    {
+        public Triangle(string Color) { color = Color; }
+        private string color;
+        public override void printShape()
+        {
+            Console.WriteLine("I am triangle");
+        }
+        public override void printColor()
+        {
+            Console.WriteLine($"I am {color}");
+        }
+    }
+    internal class RedFactory: AbstractFactory //Creates red family objects
+    {
+        public override AbstractProduct createSquare()
+        {
+            return new Square("red");
+        }
+        public override AbstractProduct createTriangle()
+        {
+            return new Triangle("red");
+        }
+    }
+    internal class GreenFactory : AbstractFactory //Creates green family objects
+    {
+        public override AbstractProduct createSquare()
+        {
+            return new Square("green");
+        }
+        public override AbstractProduct createTriangle()
+        {
+            return new Triangle("green");
+        }
+    }
+}
---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,31 @@
+﻿using Csharp_study;
+
+//Пример работы абстрактной фабрики (фабрики создают похожие объекты разных семейств)
+class Program
+{
+    static void Main(string[] args)
+    {
+        AbstractFactory RF = new RedFactory();
+        AbstractFactory GF = new GreenFactory();
+        AbstractProduct RT = RF.createTriangle();
+        AbstractProduct RS = RF.createSquare();
+        AbstractProduct GT = GF.createTriangle();
+        AbstractProduct GS = GF.createSquare();
+
+        Console.WriteLine("Red family:");
+        Console.WriteLine("\t triangles:");
+        RT.printColor();
+        RT.printShape();
+        Console.WriteLine("\t squares:");
+        RS.printColor();
+        RS.printShape();
+        Console.WriteLine("===================");
+        Console.WriteLine("Green family:");
+        Console.WriteLine("\t triangles:");
+        GT.printColor();
+        GT.printShape();
+        Console.WriteLine("\t squares:");
+        GS.printColor();
+        GS.printShape();
+    }
+}

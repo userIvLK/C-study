7dc40e9c368520c165254ab714da18782ae91a62
userIvLK <121995211+userIvLK@users.noreply.github.com>
2024-03-23 11:35:09 +0300

    Add files via upload

---
+++ ChainOfREsponsibility/WorkingExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using ChainOfREsponsibility;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //Set delivery priority
+        AbstractHandler HumanH = new Human();
+        AbstractHandler DroneH = new Drone();
+        AbstractHandler RobotH = new Robot();
+        DroneH.Successor = HumanH;
+        RobotH.Successor = DroneH;
+
+
+        Reciever Bob = new("Bob", new List<TransportationType> { TransportationType.human, TransportationType.drone });
+        Order BobsOrder = Bob.MakeAnOrder("hat");
+        RobotH.Handle(BobsOrder);
+        
+
+
+    }
+}
+

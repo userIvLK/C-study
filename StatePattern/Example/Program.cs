c7628bf24d0411b65d300a040f1b046277fbbcba
userIvLK <121995211+userIvLK@users.noreply.github.com>
2024-03-23 14:12:54 +0300

    Add files via upload

---
+++ StatePattern/Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using StatePattern;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Elevator MyElevator = new Elevator(new State1());
+        MyElevator.Move3();
+        MyElevator.Move2();
+        MyElevator.Move2();
+        MyElevator.Move1();
+    }
+}

4fa7967dc9c3d517f486c02ee4e54c95d0cdcf9f
userIvLK <121995211+userIvLK@users.noreply.github.com>
2024-03-29 08:22:19 +0300

    Add files via upload

---
+++ AdapterPattern/Example/Program.cs
@@ -0,0 +1,22 @@
+﻿using Adapter_pattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Painter Bob = new();
+        IDrawingTool pencil = new Pencil();
+        IDrawingTool chalk = new Chalk();
+        SprayPaint SP = new();
+        IDrawingTool sprayPaint = new AdapterToIDrawingTool(SP);
+
+        Bob.Paint();
+        Bob.TakeTool(pencil);
+        Bob.Paint();
+        Bob.TakeTool(chalk); 
+        Bob.Paint();
+        Bob.TakeTool(sprayPaint);
+        Bob.Paint();
+
+    } 
+}

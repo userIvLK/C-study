2def4fd4a5d6cf4bb60eaa4d4473d234da92d55d
userIvLK <121995211+userIvLK@users.noreply.github.com>
2024-03-30 13:01:41 +0300

    Add files via upload

---
+++ DI/Program.cs
@@ -0,0 +1,13 @@
+﻿using DIConsole;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ILogger MyLog = new Logger();
+        ISalary EqSalary = new EquadorSalary(MyLog);
+        ITax EqTax = new EquadorTax(MyLog);
+        SalaryCalculator SC = new(EqSalary, EqTax);
+        Console.WriteLine(SC.CalcNetSalary().Amount);
+    }
+}
---
+++ DI/SalaryCalculator.cs
@@ -0,0 +1,19 @@
+﻿namespace DIConsole
+{
+    public class SalaryCalculator
+    {
+        private readonly ISalary _salary;
+        private readonly ITax _tax;
+        public SalaryCalculator(ISalary salary, ITax tax)
+        {
+            _salary = salary;
+            _tax = tax;
+        }
+
+        public SalaryValue CalcNetSalary()
+        {
+            SalaryValue sal = _salary.GrosSalary;
+            return new SalaryValue(sal.Amount - (sal.Amount * _tax.Tax / 100), sal.Currency);
+        }
+    }
+}

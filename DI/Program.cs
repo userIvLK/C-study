a1ca7f448ae0eab5156ed6cecf4ff5f718e6e9b4
userIvLK <121995211+userIvLK@users.noreply.github.com>
2024-04-02 22:12:45 +0300

    ChangedProgramFile

--- DI/Program.cs
+++ DI/Program.cs
@@ -1,13 +1,41 @@
 ﻿using DIConsole;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data.SqlTypes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        ILogger MyLog = new Logger();
-        ISalary EqSalary = new EquadorSalary(MyLog);
-        ITax EqTax = new EquadorTax(MyLog);
-        SalaryCalculator SC = new(EqSalary, EqTax);
-        Console.WriteLine(SC.CalcNetSalary().Amount);
+        //ILogger MyLog = new Logger();
+        //ISalary EqSalary = new EquadorSalary(MyLog);
+        //ITax EqTax = new EquadorTax(MyLog);
+        //SalaryCalculator SC = new(EqSalary, EqTax);
+        //Console.WriteLine(SC.CalcNetSalary().Amount);
+
+        
+        var servs = new ServiceCollection()
+            .AddSingleton<ILogger, Logger>()
+            .AddSingleton<ISalary, EquadorSalary>()
+            .AddSingleton<ITax, EquadorTax>()
+            .AddSingleton<SalaryCalculator>();
+
+        using var serviceProvider = servs.BuildServiceProvider();
+
+        SalaryCalculator? SC = serviceProvider.GetService<SalaryCalculator>();
+
+        decimal? salaryAmount;
+        salaryAmount = SC?.CalcNetSalary().Amount;
+        List<decimal?> salary = new();
+        Console.WriteLine();
+        for(int i = 0; i<3; ++i)
+        {
+            salary.Add(SC?.CalcNetSalary().Amount);
+        }
+        Console.WriteLine();
+        for(int i = 0; i<3; ++i)
+        {
+            Console.WriteLine($"month {i + 1}");
+            Console.WriteLine(salary[i]);
+        }
     }
 }
